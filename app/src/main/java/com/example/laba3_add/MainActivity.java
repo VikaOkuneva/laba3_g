728c65b91ecd04a754e5023865e8a1bd92c1be37
origin <Vika.okuneva.96@bk.ru>
2024-12-23 21:54:01 +0300

    var2

---
+++ app/src/main/java/com/example/laba3_add/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.laba3_add;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void next(View v) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
